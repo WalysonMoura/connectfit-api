c24bde6f87f33f5f514a7044bb03b937582c2bfd
{ Walyson } <walysonmoura222@gmail.com>
2023-09-28 19:50:59 +0000

    new

---
+++ src/use-cases/resgister/resgister.ts
@@ -0,0 +1,42 @@
+import { User } from "@prisma/client";
+import { UsersRepository } from "../../repositories/users-repository";
+import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "../errors/user-already-exists-error";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+    name,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const usersWithSameEmail = this.usersRepository.findByEmail(email);
+
+    if (!usersWithSameEmail) {
+      throw new UserAlreadyExistsError();
+    }
+
+    const password_hash = await hash(password, 6);
+
+    const user = await this.usersRepository.create({
+      email,
+      name,
+      password_hash,
+    });
+
+    return {
+      user,
+    };
+  }
+}
