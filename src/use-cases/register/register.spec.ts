5d87fb271ab8169a8a6dbfb75e658dbae68ef7df
{ Walyson } <walysonmoura222@gmail.com>
2024-03-07 11:17:23 +0000

    new

--- src/use-cases/register/register.spec.ts
+++ src/use-cases/register/register.spec.ts
@@ -1,12 +1,13 @@
 import { beforeEach, describe } from "vitest";
 import { InMemoryUserRepositories } from "../../repositories/in-memory/in-memory-users-repositories";
+import { RegisterUseCase } from "./register";
 
-let userRepository:InMemoryUserRepositories
-let sut
-
+let userRepository: InMemoryUserRepositories;
+let sut: RegisterUseCase;
 
 describe("Register Use Case", () => {
   beforeEach(() => {
-    userRepository
+    userRepository = new InMemoryUserRepositories();
+    sut = new RegisterUseCase(userRepository);
   });
 });
--- src/use-cases/register/register.ts
+++ src/use-cases/register/register.ts
@@ -1,8 +1,41 @@
+import { User } from "@prisma/client";
+import { UsersRepository } from "../../repositories/users-repository";
+import { hash } from "bcryptjs";
+import { UserAlreadyExists } from "../erros/user-already-exists";
 
-
-
-export class RegisterUseCase{
-async execute() {
-    
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
 }
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
+export class RegisterUseCase {
+  constructor(private userRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const userWithSameEmail = await this.userRepository.findByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExists();
+    }
+
+    const password_hash = await hash(password, 6);
+
+    const user = await this.userRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return {
+      user,
+    };
+  }
 }
