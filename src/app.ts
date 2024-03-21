4df879f3ec6002aad3907804921b8ae14f8d8425
{ Walyson } <walysonmoura222@gmail.com>
2024-03-21 17:06:51 +0000

    new

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
+import { userRoutes } from "./http/controller/user/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
-/* 
-app.register(fastifyJwt);
-app.register(fastifyCookie);
- */
+
+app.register(userRoutes);
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validate error.", issue: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
---
+++ src/http/controller/user/register.ts
@@ -0,0 +1,3 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {}
