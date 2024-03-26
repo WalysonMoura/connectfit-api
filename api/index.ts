2196a23d701c99cbec1af928c8b8385f5025c09d
{ Walyson } <walysonmoura222@gmail.com>
2024-03-26 08:35:07 -0400

    new

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "@/app";
-import { FastifyRequest, FastifyReply } from "fastify";
-
-
-  export default async function handler(request: FastifyRequest, reply: FastifyReply) {
-    await app.ready()
-    app.server.emit('request', request, reply)
-  }
