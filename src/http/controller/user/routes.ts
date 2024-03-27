aed2426132b280b858bc50bfb30896a0c73775dc
{ Walyson } <walysonmoura222@gmail.com>
2024-03-27 20:43:51 +0000

    new

--- src/http/controller/user/routes.ts
+++ src/http/controller/user/routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
+import { profile } from "./profile";
 
 export async function userRoutes(app: FastifyInstance) {
-  app.post("/user", register);
+  app.post("/user", profile);
   app.post("/sessions", authenticate);
 }
