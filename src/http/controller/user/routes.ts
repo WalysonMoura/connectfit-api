72753a9152fd478557267bd9bbbcb00bf4b9a81c
{ Walyson } <walysonmoura222@gmail.com>
2024-03-29 14:34:39 +0000

    new

--- src/http/controller/user/routes.ts
+++ src/http/controller/user/routes.ts
@@ -3,7 +3,15 @@ import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { profile } from "./profile";
 
+import { refresh } from "./refresh";
+import { verifyTwt } from "@/http/middlewares/verify-jwt";
+
 export async function userRoutes(app: FastifyInstance) {
   app.post("/user", profile);
   app.post("/sessions", authenticate);
+
+  app.patch("token/refresh", refresh);
+
+  // Authenticated
+  app.get("/me", { onRequest: [verifyTwt] }, profile);
 }
